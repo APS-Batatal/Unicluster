6c88c2c297b62bbdc88afb34e0e8e41779331934
Diego Lopes <dihgg@dihgg.com>
2015-10-25 13:34:29 -0200

    modificações no sass, limpeza do bones && configuração do gulp/compass

--- www/wp-content/themes/unicluster/templates/home-template.php
+++ www/wp-content/themes/unicluster/templates/home-template.php
@@ -1,6 +1,5 @@
 <?php /* Template Name: Home */ ?>
 <?php get_header(); ?>
 <section id="home">
-	teste
 </section>
 <?php get_footer(); ?>
---
+++ www/wp-content/themes/unicluster/templates/projects-template.php
@@ -0,0 +1,2 @@
+<?php /* Template Name: Projetos*/ ?>
+<?php $page = reset(get_posts(array('name'=>'servicos','post_type' => 'page')));?>
