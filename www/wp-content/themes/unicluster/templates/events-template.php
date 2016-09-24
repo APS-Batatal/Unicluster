127f4426e8254e848dd87686e1d50f06823af642
Diego Lopes <dihgg@dihgg.com>
2016-09-24 20:39:37 -0300

    correções

--- www/wp-content/themes/unicluster/templates/events-template.php
+++ www/wp-content/themes/unicluster/templates/events-template.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: Projetos*/ ?>
+<?php /* Template Name: Eventos*/ ?>
 <?php
 /*
  * CUSTOM POST TYPE ARCHIVE TEMPLATE
@@ -16,33 +16,33 @@
 <?php wp_enqueue_script( 'archive', get_stylesheet_directory_uri() . '/library/min/js/pages/archive/archive.js', array( 'jquery' ), '', true ); ?>
 <?php get_header(); ?>
 	<?php  
-		$total = wp_count_posts('projects');
+		$total = wp_count_posts('events');
 		$total = $total->publish;
 		$args = array(
-			'post_type' => 'projects',
+			'post_type' => 'events',
 			'posts_per_page' => $total,
 			'orderby'=>'post_date',
 			'order'=>'DESC'
 		)?>
-	<?php $projects = get_posts($args);?>
+	<?php $events = get_posts($args);?>
 			<div id="content">
 				<div id="inner-content" class="wrap cf">
 					<main id="main" class="m-all t-all d-all cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-						<h1 class="archive-title h2">Projetos</h1>
+						<h1 class="archive-title h2">Eventos</h1>
 						<ul>
-							<?php foreach($projects as $index => $project):  ?>
+							<?php foreach($events as $index => $event):  ?>
 								<li class="m-all t-1of2 d-1of3 h-1of4">
-									<article id="post-<?php echo $project->ID ?>" <?php post_class( 'cf' ); ?> role="article" style="background-color:<?php echo get_field('category_color', $project->ID) ?>">
-										<a href="<?php echo get_post_permalink($project->ID) ?>"></a>
+									<article id="post-<?php echo $event->ID ?>" <?php post_class( 'cf' ); ?> role="article" style="background-color:<?php echo get_field('category_color', $event->ID) ?>">
+										<a href="<?php echo get_post_permalink($event->ID) ?>"></a>
 										<header class="article-header">
-											<h3 class="h2"><?php echo $project->post_title ?></h3>
+											<h3 class="h2"><?php echo $event->post_title ?></h3>
 										</header>
-										<?php $featuredImage = wp_get_attachment_image_src( get_post_thumbnail_id($project->ID), 'large' )['0'] ?>
+										<?php $featuredImage = wp_get_attachment_image_src( get_post_thumbnail_id($event->ID), 'large' )['0'] ?>
 										<?php if(!is_null($featuredImage)): ?>
 											<img src="<?=$featuredImage?>"	style="width:100%;height:auto" />
 										<?php endif ?>
 										<section class="entry-content cf">
-											<p><?php echo (strlen($project->post_content)>180)? (substr($project->post_content,0,180)."...") : $project->post_content; ?></p>
+											<p><?php echo (strlen($event->post_content)>180)? (substr($event->post_content,0,180)."...") : $event->post_content; ?></p>
 										</section>
 									</article>
 								</li>
